aed28c76fa08c4a32f7c1f02513429193cce2b3c
Jake Ramirez <jake_ramirez26@hotmail.com>
2024-12-17 12:33:10 -0500

    improved collision logic

--- Assets/Scripts/Enemy_Movement.cs
+++ Assets/Scripts/Enemy_Movement.cs
@@ -5,15 +5,19 @@ using UnityEngine;
 // Define the Enemy_Movement class, which inherits from MonoBehaviour
 public class Enemy_Movement : MonoBehaviour
 {
-    // Declare a Transform variable to hold the player's Transform component
     public Transform player;
-    // Declare a float variable to set the enemy's movement speed
+    Rigidbody2D rb;
     public float moveSpeed = 2f;
-    // Declare a float variable to set the distance at which the enemy starts following the player
     public float followDistance = 5f;
     bool alert = false;
     public float distMult = 1.5f;
     float previousSpeed;
+
+    void Start()
+    {
+        player = GameObject.FindGameObjectWithTag("Player").transform;
+        rb = GetComponent<Rigidbody2D>();
+    }
 
     // Update is called once per frame
     void Update()
@@ -25,29 +29,40 @@ public class Enemy_Movement : MonoBehaviour
         {
             followPlayer();
             alert = true;
-        }else if(distanceToPlayer < followDistance * distMult && alert){
+        }
+        else if (distanceToPlayer < followDistance * distMult && alert)
+        {
             followPlayer();
-        }else{
+        }
+        else
+        {
             alert = false;
         }
         Debug.Log(moveSpeed);
-        
     }
-    void followPlayer(){
+
+    void followPlayer()
+    {
         // Calculate the direction from the enemy to the player and normalize it
         Vector2 direction = (player.position - transform.position).normalized;
         // Move the enemy towards the player at the specified movement speed
         transform.position = Vector2.MoveTowards(transform.position, player.position, moveSpeed * Time.deltaTime);
     }
 
-    public void InvokeStop(){
-        CancelInvoke("RestoreMovement");
+    public void InvokeStop()
+    {
+        StopAllCoroutines();
         previousSpeed = moveSpeed;
         moveSpeed = 0;
-        Invoke("RestoreMovement", 1.5f);
+        StartCoroutine(RestoreMovement());
     }
-    void RestoreMovement(){
+
+    IEnumerator RestoreMovement()
+    {
+        yield return new WaitForSeconds(1.5f);
         moveSpeed = previousSpeed;
     }
+
+    
 }
 
